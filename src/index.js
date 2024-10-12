db6135f724c84dcbd8ab4e25f86d234c57249694
Akshat Pandey <akshatp1903@gmail.com>
2024-10-12 12:58:31 +0530

    describing utils (api res/err)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
     path: './env' 
 })
 
-connectDB();   
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("DB connect failed!");
+})  
 
 
 
